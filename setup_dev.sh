8a575975019bccb9a34b8536e32000036a10ac78
hzzoky <zhaozhan.he@orcadt.com>
2020-09-09 18:20:02 +0800

    modify setup_dev.sh

--- setup_dev.sh
+++ setup_dev.sh
@@ -18,6 +18,8 @@ fi
 
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     eval $RUN_SUDO cp -f ${PWD}/faiss/libfaiss.a /usr/local/lib/libfaiss.a
+    eval $RUN_SUDO ldconfig
 fi
 
+eval $RUN_SUDO rm -r /usr/include/faiss
 eval $RUN_SUDO ln -s ${PWD}/faiss /usr/include/faiss
